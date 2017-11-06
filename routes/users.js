fa89b205ba0c4728c893c6e6f26e24886cf044c5
scue <linkscue@gmail.com>
2017-11-06 09:15:59 +0800

    接收路径参数成功

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+// var express = require("express");
+// var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// /* GET users listing. */
+// router.get("/", function(req, res, next) {
+//     res.send("respond with a resource");
+// });
+
+// module.exports = router;
+
+import express, { router } from "express";
+
+let users = express.Router();
+users.get("/", (req, res, next) => {
+    res.send("respond with a resource");
 });
 
-module.exports = router;
+let user = express.Router();
+user.get("/:username", (req, res, next) => {
+    res.send("respond with a user: " + req.params.username);
+});
+
+
+export { users, user };
